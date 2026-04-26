147eca4cda09ab8fe56c54279fe3c44967f82ec2
JerMa25 <tchamijerry@gmail.com>
2026-04-26 05:24:44 +0100

    db.ts config

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,11 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const pool = new Pool({
-  host: "localhost",
+  host: "dpg-d7miuscvikkc73803gug-a.oregon-postgres.render.com",
   port: 5432,
-  database: "hackverse_db",
-  user: "admin",
-  password: "password",
+  database: "hackverse_db_y47m",
+  user: "hackverse_db_y47m_user",
+  password: "8HHVrC0SDVh4JwK6g3baa0SYt3OhEx3u",
   ssl: {
     rejectUnauthorized: false,
   },
